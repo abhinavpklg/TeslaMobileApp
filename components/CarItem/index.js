af3672b9f516b788da7cc1c26d33cbcc072ec427
abhinav <abhinavp.klg@gmail.com>
2021-01-29 16:22:44 +0530

    added buttons and background image

---
+++ components/CarItem/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import styles from './styles'
+import StyledButton from '../StyledButton'
+
+const CarItem = (props) => {
+    return (
+        <View style={styles.carContainer}>
+        <ImageBackground source={require('../../assets/images/ModelX.jpeg')}
+        style={styles.image}
+        />
+
+          <View style={styles.titles}>
+            <Text style={styles.title}>Model S</Text>
+            <Text style={styles.subtitle}>Starting at $69,420</Text>
+          </View>
+          <StyledButton type="primary"
+          content={"Custom Order"}
+          onPress={()=>{
+              console.warn("custom order button pressed");
+          } }/>
+          <StyledButton type="secondary"
+          content={"Existing Inventory"}
+          onPress={()=>{
+              console.warn("Existing Inventory button pressed");
+          } }/>
+      </View>
+    )
+}
+
+export default CarItem
+
